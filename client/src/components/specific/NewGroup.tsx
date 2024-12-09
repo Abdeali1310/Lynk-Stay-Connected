a2a2cbab3d8b5b7f8116620fd150967f4601c247
unknown <abdealidahodwala163@gmail.com>
2024-12-09 22:22:20 +0530

    Designing New Group Dialog Box started

--- client/src/components/specific/NewGroup.tsx
+++ client/src/components/specific/NewGroup.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { sampleUsers } from "../constants/sampleData";
+import UserItem from "../shared/UserItem";
+import { useInputValidation } from "6pp";
 
 const NewGroup = () => {
-  return (
-    <div>NewGroup</div>
-  )
-}
+  const groupName = useInputValidation("");
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMembers, setSelectedMembers] = useState([]);
 
-export default NewGroup
+  const selectMemberHandler = (id) => {
+    setSelectedMembers((prev: any) =>
+      prev.includes(id)
+        ? prev.filter((currentElement) => currentElement !== id)
+        : [...prev, id]
+    );
+  };
+
+  console.log(selectedMembers);
+
+  const submitHandler = () => {};
+
+  return (
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "3rem" }} width={"25rem"} spacing={"2rem"}>
+        <DialogTitle textAlign={"center"} variant="h4">
+          New Group
+        </DialogTitle>
+
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+
+        <Typography variant="body1">Members</Typography>
+
+        <Stack>
+          {members.map((user) => (
+            <UserItem
+              user={user}
+              key={user._id}
+              handler={selectMemberHandler}
+            />
+          ))}
+        </Stack>
+
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <Button variant="outlined" color="error" size="large">
+            Cancel
+          </Button>
+          <Button variant="contained" size="large" onClick={submitHandler}>
+            Create
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default NewGroup;
